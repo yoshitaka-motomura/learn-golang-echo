75d65064c041a0f9bcf5719616a680da2cb6a4e2
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-11-10 18:05:56 +0900

    データベース接続機能を追加し、マイグレーションを実行するように変更。Docker Compose設定を更新し、MySQLサービスを追加。go.modに必要な依存関係を追加。

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"log"
 	"log/slog"
 	"os"
 
 	"github.com/yoshitaka-motomura/learn-golang-echo/config"
+	"github.com/yoshitaka-motomura/learn-golang-echo/internal/database"
 	"github.com/yoshitaka-motomura/learn-golang-echo/internal/logging"
+	"github.com/yoshitaka-motomura/learn-golang-echo/internal/models"
 	"github.com/yoshitaka-motomura/learn-golang-echo/internal/server"
 )
 
@@ -15,10 +16,13 @@ func main() {
     logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
     logging.InitLogger(logger)
 
+    database.Connect()
+
+    database.MigrateDB(&models.Todo{})
+
     s := server.NewServer(logging.Logger(), true)
 
-    log.Printf("Starting server on port %s...", cfg.Port)
     if err := s.Start(":" + cfg.Port); err != nil {
-        log.Fatalf("Failed to start server: %v", err)
+        logging.Logger().Error("Failed to start server", "error", err)
     }
 }
